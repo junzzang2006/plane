cc647ee9b8a8af89754ce1fc9715596dd41fd458
junzzang2006 <junzzang2006@gmail.com>
2025-06-26 03:35:39 +0900

    6/26

--- src/ReservationSystem.java
+++ src/ReservationSystem.java
@@ -1,13 +1,65 @@
 public class ReservationSystem {
-    public static void main(String[] args) {
-    }
+    static String[][] Seat;
+    static String Blank = "                                                            ";
     public static void makeSeats(int row, int col) {
-        String[][] Seat = new String[row][col];
-        for (int i = 65+row; i >= 65; i--) {
+        Seat = new String[row][col];
+        for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                Seat[i][j] =String.valueOf(j+(char)i);
+                // 각 좌석을 "A1", "A2" 형식으로 저장
+                Seat[i][j] =  String.valueOf(j + 1) + (char)(65 + i);
             }
         }
     }
+    public static void displaySeat(String systemTitle) {
+        for (int i = Seat.length-1; i >= 0; i--) {
+            for (int j = 0; j < Seat[i].length; j++) {
+                System.out.print('[' + "%3s".formatted(Seat[i][j]) +']'+ ' ');
+            }
+            System.out.println("\n"+Blank);
+            if (i == 2){
+                System.out.println("-----------------------------------------------------------\n" +
+                        "앞\t\t\t\t\t\t\t통로\t\t\t\t\t\t\t\t뒤\n" +
+                        "-----------------------------------------------------------\n");
+            }
+        }
+
+    }
+
+    public static boolean reserveSeat(String seatName){
+
+        return true;
+    }
+
+    public static boolean cancelSeat(String seatName){
+
+        return true;
+    }
+
+    public static boolean	getSeatIndex(String	seatName,int seatArray[]){
+        String num = seatName.substring(0,seatName.length()-1);
+
+        if ((seatName.length() >= 4) || (seatName.charAt(seatName.length()-1) < 65 )|| (seatName.charAt(seatName.length()-1) > 68 || Integer.valueOf(num) > Seat[0].length)){
+            System.out.println("좌석번호: 잘못된 좌석 번호입니다");
+        }
+        else{
+            if (seatName.length() == 2){
+                seatArray[0] = Seat.length - seatName.charAt(1) + 64;
+                seatArray[1] = seatName.charAt(0) - 49;
+                System.out.println(seatArray[0] + " " + seatArray[1]);
+            }
+            if (seatName.length() == 3){
+                seatArray[0] = Seat.length - seatName.charAt(2) + 64;
+                seatArray[1] = Integer.valueOf(num) - 1;
+                System.out.println(seatArray[0] + " " + seatArray[1]);
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        makeSeats(4, 10);
+        displaySeat("_");
+    }
+
 
 }
